1afacb1d267ebb265248c900657d5ce3ba39303a
just_pr()grammer <avazbekabdurafikov21@gmail.com>
2024-05-08 23:31:01 +0500

    Added total price, and image

--- main.py
+++ main.py
@@ -41,5 +41,18 @@ for filepath in filepaths:
         pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), ln=1, border=1)
+    total_sum = df["total_price"].sum()
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=60, h=8, txt=" ", border=1)
+    pdf.cell(w=40, h=8, txt=" " , border=1)
+    pdf.cell(w=30, h=8, txt=" " , border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), ln=1, border=1)
+
+    pdf.set_font(family="Times", size=10,style='B')
+    pdf.cell(w=30, h=8, txt=f"The total price is ${total_sum:}",ln=1)
+
+    pdf.set_font(family="Times", size=14,style="B")
+    pdf.cell(w=25, h=8, txt="PythonHow")
+    pdf.image("pythonhow.png",w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
